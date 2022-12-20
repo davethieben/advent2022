e8637eee62788c727a2f498dd3b8d907d7d2f8b4
Dave Thieben <davethieben@filevine.com>
2022-12-20 11:38:07 -0500

    day 3 part 1

---
+++ day03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Essentials;
+
+const string priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+int GetPriority(char x)
+{
+    for (int index = 0; index < priorities.Length; index++)
+    {
+        if (priorities[index] == x) return index + 1;
+    }
+    return 0;
+}
+
+var inputs = File.ReadAllText("./data/input.txt")
+                 .Split('\n')
+                 .Select(line => (line?.Trim()) ?? string.Empty);
+
+Debug.WriteLine($"Found {inputs.Count()} inputs");
+
+int totalSum = 0;
+
+foreach (var input in inputs)
+{
+    var items = input.ToArray();
+    var itemCount = items.Length / 2;
+
+    var itemsA = input.Take(itemCount).ToArray();
+    var itemsB = input.Skip(itemCount).Take(itemCount).ToArray();
+
+    var commonItems = itemsA.Where(i => itemsB.Contains(i)).Distinct().ToArray();
+    Debug.WriteLine($" - Common items: {commonItems.ToDisplayString()}");
+
+    int inputSum = 0;
+    foreach (var item in commonItems)
+        inputSum += GetPriority(item);
+
+    totalSum += inputSum;
+    Debug.WriteLine($"Current Sum: {inputSum}; Total Sum: {totalSum}");
+}
+
+
